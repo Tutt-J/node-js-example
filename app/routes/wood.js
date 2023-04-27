ad3ed8a071a4a82a495527878a8235c871270517
Wy <wy@mail.com>
2023-04-27 15:42:48 +0200

    require env

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const woodCtrl = require("../controllers/wood.js");
 const auth = require("../middleware/auth.js")
 const multer  = require('../middleware/multer-local.js')
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
     multer  = require('../middleware/multer-s3.js')
 }

9d7682a833a5fd53baab2a6f2c992e37d884a473
Wy <wy@mail.com>
2023-04-27 15:44:49 +0200

    correct const to let

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -3,8 +3,7 @@ const express = require("express");
 const router = express.Router();
 const woodCtrl = require("../controllers/wood.js");
 const auth = require("../middleware/auth.js")
-const multer  = require('../middleware/multer-local.js')
-console.log(process.env.NODE_ENV)
+let multer  = require('../middleware/multer-local.js')
 if (process.env.NODE_ENV === 'production') {
     multer  = require('../middleware/multer-s3.js')
 }

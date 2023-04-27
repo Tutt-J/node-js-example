8bb73aeaf98dcee31508f5228ad249d53278ae5c
Wy <wy@mail.com>
2023-04-27 15:45:56 +0200

    inverse condition

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -3,9 +3,9 @@ const express = require("express");
 const router = express.Router();
 const woodCtrl = require("../controllers/wood.js");
 const auth = require("../middleware/auth.js")
-let multer  = require('../middleware/multer-local.js')
-if (process.env.NODE_ENV === 'production') {
-    multer  = require('../middleware/multer-s3.js')
+let multer  = require('../middleware/multer-s3.js')
+if (process.env.NODE_ENV !== 'production') {
+    multer  = require('../middleware/multer-local.js')
 }
 
 router.get("/", auth, woodCtrl.readAll);
